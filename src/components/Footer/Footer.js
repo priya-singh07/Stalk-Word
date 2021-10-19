534e20c317847d7f2a56f642dff034e26f9c1044
Priya Singh <priyapragya0710@gmail.com>
2021-10-19 11:36:07 +0530

    changes in footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Footer.css';
 import FacebookIcon from '@material-ui/icons/Facebook';
-import YouTubeIcon from '@material-ui/icons/YouTube';
+import GitHubIcon from '@material-ui/icons/GitHub';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import {Link} from 'react-router-dom';
 
@@ -13,19 +13,10 @@ const Footer = () => {
                 Made by Priya Singh
             </span>
             <div className="iconContainer">
-                {/* <a href="" target="_blank">
-                    {<i className="fab fa-facebook-square fa-2x"></i>}
-                </a>
-                <a href="" target="_blank">
-                    <i className="fab fa-linkedin fa-2x"></i>
-                </a>
-                <a href="" target="_blank">
-                    <i className="fab fa-youtube fa-2x"></i>
-                </a> */}
 
-                <a href="#" className="link"><YouTubeIcon/></a>
-              <a href="#" className="link"><FacebookIcon /></a>
-              <a href="#" className="link"><LinkedInIcon /></a>
+                <a href="https://github.com/priya-singh07" className="link" target="_blank"><GitHubIcon/></a>
+              <a href="https://www.facebook.com/profile.php?id=100041087550369" className="link" target="_blank"><FacebookIcon /></a>
+              <a href="https://www.linkedin.com/in/priya-singh-1b3124201/" className="link" target="_blank"><LinkedInIcon /></a>
             </div>
         </div>
     );
